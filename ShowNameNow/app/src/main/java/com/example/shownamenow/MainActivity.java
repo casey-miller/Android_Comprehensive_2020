eb3c8ea262d3f6d6e89d6b8024c0e018d4e1600b
Casey Miller <caseymiller@arizona.edu>
2020-06-24 07:50:55 -0700

    First app - in progress

---
+++ ShowNameNow/app/src/main/java/com/example/shownamenow/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.shownamenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button myButton;
+    private TextView showText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showText.setText("Hello I am here!");
+            }
+        });
+        showText = findViewById(R.id.textView);
+
+
+
+
+    }
+}
